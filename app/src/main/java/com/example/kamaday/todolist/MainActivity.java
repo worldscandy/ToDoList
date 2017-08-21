427e7ac030f34d8ffa4eb0cf0a9bb666754b5fa5
worldscandy <fyuken322@gmail.com>
2017-08-21 16:13:13 +0900

    ToDoList　基本的なxmlレイアウトの設置完了

--- app/src/main/java/com/example/kamaday/todolist/MainActivity.java
+++ app/src/main/java/com/example/kamaday/todolist/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.kamaday.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+
+    public static CheckBox ToDoList;
+    public static Button inputListButton;
+    public static EditText inputListText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToDoList = (CheckBox) findViewById(R.id.ToDoList);
+        inputListButton = (Button) findViewById(R.id.inputListButton);
+        inputListText = (EditText) findViewById(R.id.inputListText);
     }
 }
